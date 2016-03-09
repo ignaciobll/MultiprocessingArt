f98b734b670bd38a6881d11e5dd52b3efd9545b7
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-03-10 00:58:26 +0100

    Problema 4. AoMP. Readme migrado a org ♥

---
+++ Presos.java
@@ -0,0 +1,135 @@
+import java.util.concurrent.Semaphore;
+
+public class Presos {
+
+    private static final int N_PRESOS = 10;
+
+    static Semaphore sem = new Semaphore(1);
+
+    public static volatile boolean fin = false;
+
+    public static void main(String[] args) {
+
+	Switch light = new Switch();
+
+	Jefe jefe = new Jefe(light);
+	Preso [] presos = new Preso [N_PRESOS];
+
+	for (int i = 0; i < N_PRESOS; i++) {
+	    presos[i] = new Preso(light);
+	}
+
+	jefe.start();
+	for (int i = 0; i < N_PRESOS; i++) {
+	    presos[i].start();
+	}
+	
+	try {
+	    for (int i = 0; i < N_PRESOS; i++) {
+		presos[i].join();
+	    }
+	    jefe.join();
+	} catch (Exception ex) {
+	    System.out.println(":(");
+	}	
+    }
+
+        static class Switch {
+	boolean status = false;
+
+	public Switch () {
+	    this.status = false;
+	}
+
+	public void throw_off(){
+	    this.status = false;
+	    System.out.println("Apagado");
+
+	}
+
+	public void throw_on(){
+	    this.status = true;
+	    System.out.println("Encendido");
+
+	}
+
+	public boolean get(){
+	    return this.status;
+	}
+    }
+
+    static class Jefe extends Thread {
+	private Switch light;
+	private int contador = N_PRESOS;
+
+	public Jefe (Switch light) {
+	    this.light = light;
+	}
+
+	public void run(){
+	    while (contador > 0){
+		try {
+		    sem.acquire(); //Acceso a sección crítica		
+		}
+		catch (Throwable e) {
+		    System.out.println("Error " + e.getMessage());
+		    e.printStackTrace();
+		}
+		
+		if (!light.get()){ //Si el switch está apagado
+		    light.throw_on();
+		    contador--;
+		    System.out.println("Quedan " + contador + " presos");
+
+		}
+		
+		try {
+		    sem.release(); //Salida de sección crítica		
+		}
+		catch (Throwable e) {
+		    System.out.println("Error " + e.getMessage());
+		    e.printStackTrace();
+		}
+	    
+	    }
+	    Presos.fin = true;
+	    System.out.println("Ya lo hemos visitado todos");
+	}
+
+    }
+    
+    static class Preso extends Thread {
+	private Switch light;
+	private boolean done = false;
+	
+	public Preso (Switch light) {
+	    this.light = light;
+	}
+
+	public void run() {
+	    while (!Presos.fin){
+		try {
+		    sem.acquire();	
+		}
+		catch (Throwable e) {
+		    System.out.println("Error " + e.getMessage());
+		    e.printStackTrace();
+		}
+
+		if (light.get() && !done){
+		    light.throw_off();
+		    done = true;
+		}
+
+		try {
+		    sem.release();		
+		}
+		catch (Throwable e) {
+		    System.out.println("Error " + e.getMessage());
+		    e.printStackTrace();
+		}
+	    }
+	}
+    }
+}
+    
